4199563cb0c304c0f5eb1579ea79dc3d0b1244e9
egbertcw0711 <egbertcw@umich.edu>
2017-06-25 22:52:05 -0400

    compatible to process both ZigBee and WiFi data

--- MATLAB/readData.m
+++ MATLAB/readData.m
@@ -2,8 +2,7 @@
 %  The file read the data from the experiment we did from the data folder 
 % ======================================================================= %
 
-mean_delay = [200, 100, 50, 30, 20, 10, 5, 4, 3 , 2, 1, .5, .3, .1, 0]';
-total_transmission = 1e3 * [.5, .5, .5, .5, 5, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50]';
+mean_delay = [200, 100, 50, 30, 20, 10, 5, 4, 3 , 2, 1, .5, .3, .1]';
 
 [num_data,~] = size(mean_delay);
   
