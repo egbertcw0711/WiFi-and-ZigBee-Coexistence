14e8ac26f6fef532baf405be861f5246552d7055
egbertcw0711 <egbertcw@umich.edu>
2017-06-26 00:15:23 -0400

    The latest plot ZigBee Throughput vs Expo delay

--- MATLAB/plotZigBeePLRandThroughputvsExpoDelay.m
+++ MATLAB/plotZigBeePLRandThroughputvsExpoDelay.m
@@ -1,27 +1,29 @@
 close all
 
-% load('constDelay.mat') % load the corresponding data in the folder
-total_transmission = 500 * ones(14,1);
-% plot WiFi Throughput vs Constant Delay at the end of WiFi transmission
+% load the corresponding data in the folder
+% load('WiFi600BexponentialDelay.mat')
+
+total_transmission = 500 * ones(14,1); % total transmission packets 
+
+% plot ZigBee Throughput vs Exponential Delay
 figure,
-semilogx(mean_delay, mean_throughput, 'bo--')
-xlabel('constant delay (ms)')
-ylabel('WiFi throughput (kbps)')
-title('WiFi Throughput vs. Constant Delay Time')
-% legend('Packet Size = 1112B')
+semilogx(mean_delay, mean_throughput, 'r*--')
+xlabel('exponential delay (ms)')
+ylabel('ZigBee throughput (kbps)')
+title('ZigBee Throughput vs. Exponential Delay Time')
 legend('Packet Size = 600B')
 grid on   
 set(gca, 'XDir', 'reverse') % reverse the X-AXIS
 
-% calculate the packet error rate
+% calculate the packet error rate (%)
 PLR = 100 * (total_transmission - num_rcvd_packets) ./ total_transmission;
 
-% plot WiFi Packet Error Rate vs Constant Delay at the end of WiFi transmission
+% plot ZigBee Packet Error Rate vs Constant Delay
 figure,
-semilogx(mean_delay, PLR, 'bo--')
-xlabel('constant delay (ms)')
-ylabel('WiFi packet error rate (%)')
-title('WiFi Packet Error Rate vs. Constant Delay Time')
-legend('Packet Size = 1112B')
+semilogx(mean_delay, PLR, 'r*--')
+xlabel('exponential delay (ms)')
+ylabel('ZigBee packet error rate (%)')
+title('ZigBee Packet Error Rate vs. Exponential Delay Time')
+legend('Packet Size = 600B')
 grid on   
 set(gca, 'XDir', 'reverse') % reverse the X-AXIS
