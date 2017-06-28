aaf9b20cbf6085026add9b5e4f26062446c46f4c
egbertcw0711 <egbertcw@umich.edu>
2017-06-28 17:01:34 -0400

    add more functionalities to monitor WiFi behavior

--- Esp8266/perTransmitter.ino
+++ Esp8266/perTransmitter.ino
@@ -8,8 +8,8 @@
 /* DEVICE 1 */
 #if CONFIG == 1
 char *ssid = "ESPsoftAP_01";
-char *pass = "nickkoester_and_egbertcw";
-float dBm = 20.5; // add the transmitter power
+char *pass = "nickkoester";
+float dBm = 20.5;
 unsigned int serverPort = 4210;
 unsigned int localPort = 2390;
 #endif
@@ -17,7 +17,7 @@ unsigned int localPort = 2390;
 /* DEVICE 2 */
 #if CONFIG == 2
 char *ssid = "ESPsoftAP_02";
-char *pass = "nickkoester_and_egbertcw";
+char *pass = "nickkoester";
 float dBm = 20.5;
 unsigned int serverPort = 4220;
 unsigned int localPort = 2290;
@@ -26,12 +26,16 @@ unsigned int localPort = 2290;
 IPAddress receiverIP(0, 0, 0, 0);
 WiFiUDP Udp;
 
-//int NUM_PACKETS = 50000;
+/* TRANSMITTER PARAMETERS */
+int NUM_PACKETS = 50000;
 
 /* config different packet size for transmission */
 //const int packetSize = 1500;
 //const int packetSize = 600;
 const int packetSize = 1112;
+
+/* using different packet size to calculate the WiFi transmission rate */
+//const int packetSize = 1100;
 
 const double arrivalRate = 1;
 const int generatorSeed = 1;
@@ -39,15 +43,17 @@ const int generatorSeed = 1;
 std::string message(packetSize, 'A');
 ExponentialDist randomNum(arrivalRate, generatorSeed);
 
+int num_transmission = 0; // count number of successful transmission
 unsigned long sendPacket(IPAddress& address) {
-  if (!Udp.beginPacket(address, serverPort)) {
-    Serial.println("Error in Udp.beginPacket()");
-  }
-  
+  //if (!Udp.beginPacket(address, serverPort)) {
+    //Serial.println("Error in Udp.beginPacket()");
+  //}
+  Udp.beginPacket(address, serverPort);
   Udp.write(message.c_str());
-  
-  if (!Udp.endPacket()) {
-    Serial.println("Error in Udp.endPacket()");
+//  Udp.endPacket();
+  // check whether transmitter successfully transmitted the packets
+  if (Udp.endPacket()) {
+    ++num_transmission;
   }
 }
 
@@ -58,7 +64,7 @@ void connectToServer() {
   WiFi.disconnect();
   WiFi.mode(WIFI_STA);
   WiFi.setOutputPower(dBm);
-  delay(100);
+  delay(300);
   
   Serial.print("Attempting to connect to SSID: ");
   Serial.print(ssid);
@@ -76,6 +82,7 @@ void connectToServer() {
   if (!Udp.begin(localPort)) {
     Serial.println("UDP connection failed");
   }
+//  WiFi.printDiag(Serial);
 }
 
 void setup() {
@@ -86,28 +93,36 @@ void setup() {
   Serial.print("Using configuration: ");
   Serial.println(CONFIG);
   
+//  pinMode(16, OUTPUT);
   connectToServer();
 }
 
+int i = 0;                    // count number of sending times
+
 void loop() {
-  Serial.println("Sending packets...");
+  //Serial.println("Sending packets...");
 
   /* Here is used for testing ZigBee Throughput with and without WiFi Interference */
-   sendPacket(receiverIP); // send an packet to server
-//   delay(randomNum.generate()); // add random exponential delay
-   delay(1);                  // add constant delay
+////   digitalWrite(16, HIGH);
+//   sendPacket(receiverIP); // send an packet to server
+////   digitalWrite(16, LOW);
+//////   delay(randomNum.generate()); // add random exponential delay
+//////   delay(0.3);                      // ms, used for delay above 1ms
+//   delayMicroseconds(100);         // us, add constant delay below 1us
   /* ***************************************************************************** */
   
   /* Here is used for testing WiFi Throughput with and without ZigBee Interference */
-//  for(int i = 0; i < NUM_PACKETS; ++i) {
-//      sendPacket(receiverIP); // send an packet to server
-//      delay(randomNum.generate());
-//  }
-//
-//  Serial.println("Done");
-//  while(true) {
-//    delay(10000);
-//  }
+  if( i < NUM_PACKETS) {
+      sendPacket(receiverIP);        // send an packet to access point
+//      delay(randomNum.generate()); // exponential random delay
+      delayMicroseconds(500);      // constant delay
+//      delay(200);
+      ++i;
+  }
+  else {
+    // print how many packets are sent
+    Serial.printf("total successful transmission: %d\n", num_transmission);
+    delay(10000);
+  }
   /* **************************************************************************** */
-
-}
+} // end loop
