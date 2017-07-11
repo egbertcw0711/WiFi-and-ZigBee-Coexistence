7397a1a1293d308a7a6b3cea0a0cbaf3a57caa2f
Chen Wang <egbertcw@umich.edu>
2017-07-11 18:46:03 -0400

    WiFi ZigBee coexistence experiment

--- MATLAB/WIFI512underZB0710.m
+++ MATLAB/WIFI512underZB0710.m
@@ -3,49 +3,59 @@
 % the data are in the WiFiUnderZB/ and WiFiNoZB/ folder, and corresponding
 % mat file is in the same folder called the same name as the script. 
 %-------------------------------------------------------------------------%
-% related file: WIFI512underZB0710.mat, WIFI512noZB0710.mat
+% related file: WIFI512underZB0710.mat, WIFI512underZB100B25ms_0711.mat,
+%               WIFI512noZB0710.mat
 % related experiment data: WiFiUnderZB/   and   WiFiNoZB/
 %=========================================================================%
 
 close all
-wifi_delay = [0.1, 0.5, 5, 20, 50, 100, 200]';
+wifi_delay = [0.1, 5, 20, 50, 100, 200]';
 
 %% read the experiment data with interference
-wifi_throughput_interference = zeros(7,1);
-wifi_rcvd_pkts_interference = zeros(7,1);
+wifi_throughput_interference = zeros(6,1);
+wifi_rcvd_pkts_interference = zeros(6,1);
 
-for i = 1:5
+for i = 1:6
     [wifi_throughput_interference(i), wifi_rcvd_pkts_interference(i)] = ...
         get_mean_throughput();
 end
 
-wifi_throughput_interference(6) = 40.96;
-wifi_throughput_interference(7) = 20.48;
-wifi_rcvd_pkts_interference(6) = 500;
-wifi_rcvd_pkts_interference(7) = 500;
+filename = input('enter the filename: ');
+save(filename)
 
 %% read the experiment data no interference
-wifi_throughput_no_interference = zeros(7,1);
-wifi_rcvd_pkts_no_interference = zeros(7,1);
+wifi_throughput_no_interference = zeros(6,1);
+wifi_rcvd_pkts_no_interference = zeros(6,1);
 
-for i = 1:5
+for i = 1:4
     [wifi_throughput_no_interference(i), wifi_rcvd_pkts_no_interference(i)] = ...
         get_mean_throughput();
 end
 
-wifi_throughput_no_interference(6) = 40.96;
-wifi_throughput_no_interference(7) = 20.48;
-wifi_rcvd_pkts_no_interference(6) = 500;
-wifi_rcvd_pkts_no_interference(7) = 500;
+
+wifi_throughput_no_interference(5) = 40.96; % kbps
+wifi_rcvd_pkts_no_interference(5)= 500;
+wifi_throughput_no_interference(6) = 20.48; % kbps
+wifi_rcvd_pkts_no_interference(6)= 500;
+
+filename = input('enter the filename: ');
+save(filename)
 
 %% visualization the data
+load('WIFI512underZB10B25ms_0711.mat')
+wifi_throughput_interference_10 = wifi_throughput_interference;
+load('WIFI512underZB100B25ms_0711.mat')
+wifi_throughput_interference_100 = wifi_throughput_interference;
+
 figure,
-plot(wifi_delay, wifi_throughput_interference, 'bo-', ...
-    wifi_delay, wifi_throughput_no_interference, 'r*--')
+loglog(wifi_delay, wifi_throughput_interference_10, 'bo-', ...
+    wifi_delay, wifi_throughput_interference_100,'k^--', ...
+    wifi_delay, wifi_throughput_no_interference, 'r*:')
 xlabel('constant delay (ms)')
 ylabel('WiFi throughput (kbps)')
 title('WiFi Throughput vs. Constant Delay Time')
-legend('WiFi Throughput Under ZigBee Interefence',...
-    'WiFi Throughput No Interference')
+legend('Zigbee Packet Size 10byte',...
+    'ZigBee Packet Size 100byte',...
+    'No ZigBee Transmission')
 grid on   
 set(gca, 'XDir', 'reverse') % reverse the X-AXIS
--- MATLAB/ZBunderWIFI512_0710.m
+++ MATLAB/ZBunderWIFI512_0710.m
@@ -1,23 +1,76 @@
 % ======================================================================= %
-% This script is stand alone, there is no need related experiment data
-% folder or the mat file. All the experiment data are typed in the matrix
+% This script visualize the zigbee behavior under WiFi interference
+% All the experiment data are stored in the ZigbeeThroughput folder
 % in the program
+% ----------------------------------------------------------------------- %
+% related file: ZigbeeThroughput\*.mat
 % ======================================================================= %
 
 close all
 
-wifi_delay = [0.1, 0.5, 5, 20, 50, 100, 200]';
+% wifi constant delay
+wifi_delay = [0.1, 5, 20, 50, 100, 200]';
 
-zigbee_throughput_no_interference = 1.09 .* ones(7,1); % kbps
-zigbee_throughput_interference = [0.725, 0.745, 0.755, 0.945, ...
-    1.04, 1.075, 1.085]; % kbps
+% experiment zigbee throughput without interference
+zigbee_throughput_no_interference_10_25ms = 3.2 .* ones(6,1); % kbps
+zigbee_throughput_no_interference_10_100ms = 0.78 .* ones(6,1);
+zigbee_throughput_no_interference_100 = 31.8 .* ones(6,1);
 
-% visualize the data
-plot(wifi_delay, zigbee_throughput_interference, 'bo-', ...
-    wifi_delay, zigbee_throughput_no_interference, 'r--')
+% load the experiment data
+load('.\ZigbeeThroughput\zigbee100byte25ms.mat')
+zigbee100byte25ms = t;
+load('.\ZigbeeThroughput\zigbee10byte25ms.mat')
+zigbee10byte25ms = t;
+zigbee10byte100ms = [0.725, 0.755, 0.945, ...
+     1.04, 1.075, 1.085] ./ 1.4; % kbps
+
+% ========================== visualize the data ========================= %
+% plot wifi throughput with different zigbee payload [10, 100] under the 
+% same transimssion interval 25ms
+figure(1),
+plot(wifi_delay, zigbee10byte25ms, 'bo-', ...
+    wifi_delay, zigbee_throughput_no_interference_10_25ms, 'r--')
 xlabel('wifi constant delay (ms)')
 ylabel('zigbee throughput (kbps)')
-legend('ZigBee Throughput under WiFi interference', ...
-    'ZigBee Throughput without WiFi interference')
+legend('ZigBee Throughput under WiFi interference: 10 byte / 25ms',...
+    'ZigBee Throughput without WiFi interference: 10 byte / 25ms')
+grid on
 
+figure(2)
+plot(wifi_delay, zigbee100byte25ms, 'bo-', ...
+    wifi_delay, zigbee_throughput_no_interference_100, 'r--')
+xlabel('wifi constant delay (ms)')
+ylabel('zigbee throughput (kbps)')
+legend('ZigBee Throughput under WiFi interference: 100 byte / 25ms', ...
+    'ZigBee Throughput without WiFi interference: 100 byte / 25ms')
+grid on
+
+% here the zigbee throughput is linear with the zigbee packet size
+% TODO: add zigbee packet size = 1byte, plot the graph
+figure(3)
+semilogy(wifi_delay, zigbee10byte25ms, 'bo-', ...
+    wifi_delay, zigbee100byte25ms, 'g^-')
+grid on
+xlabel('wifi constant delay (ms)')
+ylabel('zigbee throughput (kbps)')
+legend('ZigBee transmission rate no interference: 10 byte / 25ms',...
+    'ZigBee transmission rate no interference: 100 byte / 25ms')
+title('ZigBee transmission rate under WiFi interference')
+
+
+% plot wifi throughput with different transmission interval under the same
+% zigbee payload 10 byte
+% ------------------ %
+% TODO:              %
+% DO THE EXPERIMENT! %
+% ------------------ %
+% further more, with specific wifi constant delay time, zigbee throughput
+% is inverse of the transmission interval
+figure(4),
+semilogy(wifi_delay, zigbee10byte25ms, 'bo-', ...
+    wifi_delay, zigbee10byte100ms, 'g^-')
+xlabel('wifi constant delay (ms)')
+ylabel('zigbee throughput (kbps)')
+legend('ZigBee transmission rate no interference: 10 byte / 25ms',...
+    'ZigBee transmission rate no interference: 10 byte / 100ms')
 grid on
