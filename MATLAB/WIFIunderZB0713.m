1205b9c9945a9178bbb236d45f3a46d73f5b44e1
Chen Wang <egbertcw@umich.edu>
2017-07-17 15:56:04 -0400

    Scenario 1

--- MATLAB/WIFIunderZB0713.m
+++ MATLAB/WIFIunderZB0713.m
@@ -9,48 +9,54 @@
 close all
 
 %% read the experiment data with interference
-wifi_throughput_interference = zeros(6,1);
-wifi_rcvd_pkts_interference = zeros(6,1);
-
-for i = 1:6
-    [wifi_throughput_interference(i), wifi_rcvd_pkts_interference(i)] = ...
-        get_mean_throughput();
-end
-
-filename = input('enter the filename: ');
-save(filename)
+% wifi_throughput_interference = zeros(6,1);
+% wifi_rcvd_pkts_interference = zeros(6,1);
+% 
+% for i = 1:6
+%     [wifi_throughput_interference(i), wifi_rcvd_pkts_interference(i)] = ...
+%         get_mean_throughput();
+% end
+% 
+% filename = input('enter the filename: ');
+% save(filename)
 
 %% read the experiment data no interference
-wifi_throughput_no_interference = zeros(6,1);
-wifi_rcvd_pkts_no_interference = zeros(6,1);
-
-for i = 1:6
-    [wifi_throughput_no_interference(i), wifi_rcvd_pkts_no_interference(i)] = ...
-        get_mean_throughput();
-end
-
-filename = input('enter the filename: ');
-save(filename)
+% wifi_throughput_no_interference = zeros(6,1);
+% wifi_rcvd_pkts_no_interference = zeros(6,1);
+% 
+% for i = 1:6
+%     [wifi_throughput_no_interference(i), wifi_rcvd_pkts_no_interference(i)] = ...
+%         get_mean_throughput();
+% end
+% 
+% filename = input('enter the filename: ');
+% save(filename)
 
 %% load the data
 wifi_delay = [0.1, 5, 20, 50, 100, 200]';
 wifi_exp_delay = [20, 50, 100, 200]';
 
+total_packets = [200000, 10000, 10000, 2000, 1000, 500]';
+
 % wifi thrpughput no interference
 load('.\WiFiNoZB\WIFI128BnoInterference.mat')
 wifi128noInterference = wifi_throughput_no_interference;
+wifi128_rcvd_noInterference = wifi_rcvd_pkts_no_interference;
 
 load('.\WiFiNoZB\WIFI512BnoInterference.mat')
 wifi512noInterference = wifi_throughput_no_interference;
+wifi512_rcvd_noInterference = wifi_rcvd_pkts_no_interference;
 
 load('.\WiFiNoZB\WIFI1024BnoInterference.mat')
 wifi1024noInterference = wifi_throughput_no_interference;
+wifi1024_rcvd_noInterference = wifi_rcvd_pkts_no_interference;
 
 wifi2048noInterference = 0 .* ones(6,1);
 
 % wifi throughput with interference 
 load('.\WiFiUnderZB\WIFI128BwithInterference10B10ms.mat')
 wifi128withInterference10B10ms = wifi_throughput_interference;
+wifi128_rcvd_withInterference10B10ms = wifi_rcvd_pkts_interference;
 
 load('.\WiFiUnderZB\WIFI512underZB100B25ms_0711.mat')
 wifi512withInterference100B25ms = wifi_throughput_interference;
@@ -60,14 +66,16 @@ wifi512withInterference10B25ms = wifi_throughput_interference;
 
 load('.\WiFiUnderZB\WIFI512underZB10B10ms_0713.mat')
 wifi512withInterference10B10ms = wifi_throughput_interference;
+wifi512_rcvd_withInterference10B10ms = wifi_rcvd_pkts_interference;
 
 load('.\WiFiUnderZB\WIFI1024underZB10B10ms_0713.mat')
 wifi1024withInterference10B10ms = wifi_throughput_interference;
+wifi1024_rcvd_withInterference10B10ms = wifi_rcvd_pkts_interference;
 
 load('.\WiFiUnderZB\WIFI512expUnderZB10byte10ms.mat')
 wifi512expwithInterference10B10ms = wifi_throughput_interference;
-%% Visualization
-% WIFI throughput vs. const delay with different wifi packet size no Inter
+%% visualize no interference
+% WIFI throughput (kbps)
 figure,
 loglog(wifi_delay, wifi128noInterference, 'bo-', ...
     wifi_delay, wifi512noInterference,'c^-', ...
@@ -83,6 +91,21 @@ legend('WiFi packet size = 128 byte',...
 grid on   
 set(gca, 'XDir', 'reverse') % reverse the X-AXIS
 
+% WIFI packet error rate (%)
+wifi128LossRate = 100 * (total_packets - wifi128_rcvd_noInterference)./ total_packets;
+wifi512LossRate = 100 * (total_packets - wifi512_rcvd_noInterference)./ total_packets;
+wifi1024LossRate = 100 * (total_packets - wifi1024_rcvd_noInterference)./ total_packets;
+figure,
+semilogx(wifi_delay, wifi128LossRate, 'r*-', ...
+    wifi_delay, wifi512LossRate, 'g*-', ...
+    wifi_delay, wifi1024LossRate, 'b*-')
+legend('WiFi Packet Size 128 byte', 'WiFi Packet Size 512 byte',...
+    'WiFi Packet Size 1024 byte')
+xlabel('WiFi Const Delay (ms)')
+ylabel('WiFi Packet Error Rate (%)')
+grid on
+
+%% visualize under interference
 % WIFI512 throughput with exponential delay
 figure,
 loglog(wifi_delay, wifi512noInterference, 'c*-', ...
@@ -97,20 +120,21 @@ set(gca, 'XDir', 'reverse') % reverse the X-AXIS
 
 % WIFI 128,512,1024 Byte throughput with/without ZigBee Interference
 figure,
-loglog(wifi_delay, wifi128noInterference, 'r*-', ...
+plot(wifi_delay, wifi128noInterference, 'r*-', ...
     wifi_delay, wifi128withInterference10B10ms, 'bo-')
 hold on,
 
-loglog(wifi_delay, wifi512noInterference, 'r*--', ...
+plot(wifi_delay, wifi512noInterference, 'r*--', ...
     wifi_delay, wifi512withInterference10B10ms, 'bo--')
 
-loglog(wifi_delay, wifi1024noInterference, 'r*:', ...
+plot(wifi_delay, wifi1024noInterference, 'r*:', ...
     wifi_delay, wifi1024withInterference10B10ms, 'bo:')
-
+hold off
 xlabel('WiFi constant delay (ms)')
 ylabel('WiFi throughput (kbps)')
-legend('WiFi Packet Size 128', '','WiFi Packet Size 512', '',...
-    'WiFi Packet Size 1024', '')
+legend('WiFi Packet Size 128 no Interference', 'with Interference', ...
+    'WiFi Packet Size 512 no Interference', 'with Interference',...
+    'WiFi Packet Size 1024 no Interference', 'with interference')
 title('WiFi throughput with constant delay')
 set(gca, 'XDir', 'reverse') % reverse the X-AXIS
 grid on
@@ -129,7 +153,6 @@ legend('WiFi no Interference', 'ZigBee Packet size 10 byte', ...
 set(gca, 'XDir', 'reverse') % reverse the X-AXIS
 grid on
 
-
 figure, % with different zigbee transmission time
 loglog(wifi_delay, wifi512noInterference, 'r*-', ...
     wifi_delay, wifi512withInterference10B10ms, 'bo--', ...
@@ -142,3 +165,16 @@ legend('WiFi no Interference', 'ZigBee transmission interval 10 ms', ...
 set(gca, 'XDir', 'reverse') % reverse the X-AXIS
 grid on
 
+%% WiFi packet error rate (%)
+wifi128LossRate_Interference = 100 * (total_packets - wifi128_rcvd_withInterference10B10ms)./ total_packets;
+wifi512LossRate_Interference = 100 * (total_packets - wifi512_rcvd_withInterference10B10ms)./ total_packets;
+wifi1024LossRate_Interference = 100 * (total_packets - wifi1024_rcvd_withInterference10B10ms)./ total_packets;
+figure,
+semilogx(wifi_delay, wifi128LossRate_Interference, 'r*-', ...
+    wifi_delay, wifi512LossRate_Interference, 'g*-', ...
+    wifi_delay, wifi1024LossRate_Interference, 'b*-')
+legend('WiFi Packet Size 128 byte', 'WiFi Packet Size 512 byte',...
+    'WiFi Packet Size 1024 byte')
+xlabel('WiFi Const Delay (ms)')
+ylabel('WiFi Packet Error Rate (%)')
+grid on
---
+++ MATLAB/ZBThunderWIFI512.m
@@ -0,0 +1,195 @@
+% ======================================================================= %
+% This script visualize the zigbee behavior under WiFi interference
+% All the experiment data are stored in the ZigbeeThroughput folder
+% in the program
+% ----------------------------------------------------------------------- %
+% related file: ZigbeeThroughput\*.mat
+% ======================================================================= %
+
+close all
+% wifi constant delay
+wifi_delay = [0.1, 5, 20, 50, 100, 200]';
+wifi_exp_delay = [20, 50, 100, 200]';
+
+% experiment zigbee throughput without interference
+zigbee_throughput_no_interference_10_25ms = 3.2 .* ones(6,1); % kbps
+zigbee_throughput_no_interference_10_100ms = 0.78 .* ones(6,1);
+zigbee_throughout_no_interference_10_10ms = 7.99 .* ones(6,1);
+zigbee_throughout_no_interference_50_25ms = 16.00 .* ones(6,1);
+zigbee_throughput_no_interference_100_25ms = 31.8 .* ones(6,1);
+zigbee_throughput_no_interference_100_100ms = 8.0 .* ones(6,1);
+
+% load the experiment data
+zigbee10byte100ms = [0.725, 0.755, 0.945, ...
+     1.04, 1.075, 1.085] ./ 1.4; % kbps
+load('.\ZigbeeThroughput\zigbee10byte25ms.mat')
+zigbee10byte25ms = t;
+load('.\ZigbeeThroughput\zigbee10byte10ms.mat')
+zigbee10byte10ms = t;
+
+load('.\ZigbeeThroughput\zigbee100byte100ms.mat')
+zigbee100byte100ms = t;
+load('.\ZigbeeThroughput\zigbee100byte50ms.mat')
+zigbee100byte50ms = t;
+load('.\ZigbeeThroughput\zigbee100byte25ms.mat')
+zigbee100byte25ms = t;
+load('.\ZigbeeThroughput\zigbee50byte25ms.mat')
+zigbee50byte25ms = t;
+
+load('.\ZigbeeThroughput\zigbee_exp_10byte10ms.mat')
+zigbee_exp_10byte10ms = t;
+
+% ========================== visualize the data ========================= %
+% plot wifi throughput with different zigbee payload [10, 100] under the 
+% same transimssion interval 25ms
+figure,
+plot(wifi_delay, zigbee10byte25ms, 'bo-', ...
+    wifi_delay, zigbee_throughput_no_interference_10_25ms, 'r--')
+xlabel('wifi constant delay (ms)')
+ylabel('zigbee throughput (kbps)')
+title('Zigbee Packet size = 10 byte, transmission interval = 25ms')
+legend('ZigBee Throughput under WiFi interference',...
+    'ZigBee Throughput without WiFi interference')
+grid on
+
+figure,
+plot(1000./wifi_delay, zigbee100byte25ms, 'bo-', ...
+    1000./wifi_delay, zigbee_throughput_no_interference_100_25ms, 'r--')
+
+xlabel('wifi constant delay (ms)')
+ylabel('zigbee throughput (kbps)')
+title('Zigbee Packet size = 100 byte, transmission interval = 25ms')
+legend('ZigBee Throughput under WiFi interference', ...
+    'ZigBee Throughput without WiFi interference')
+grid on
+
+figure,
+plot(wifi_delay, zigbee10byte10ms, 'bo-', ...
+    wifi_delay, zigbee_throughout_no_interference_10_10ms, 'r--')
+xlabel('wifi constant delay (ms)')
+ylabel('zigbee throughput (kbps)')
+title('Zigbee Packet size = 10 byte, transmission interval = 10ms')
+legend('ZigBee Throughput under WiFi interference',...
+    'ZigBee Throughput without WiFi interference')
+grid on
+
+% plot both const and exponential wifi delay
+figure,
+plot(wifi_delay, zigbee_throughout_no_interference_10_10ms, 'r--',...
+    wifi_delay, zigbee10byte10ms, 'bo-', ...
+    wifi_exp_delay, zigbee_exp_10byte10ms, 'rs-')
+xlabel('wifi delay (ms)')
+ylabel('zigbee throughput (kbps)')
+title('Zigbee Packet size = 10 byte, transmission interval = 10ms')
+legend('No Interference', 'WiFi Interference Const Delay', ...
+    'WiFi Interference Exponential Delay')
+grid on
+
+%-------------------------------------------------------------------------%
+% The following 2 plots describes:
+% (1) zigbee throughput is linear with the zigbee packet size
+figure,
+semilogy(wifi_delay, zigbee10byte25ms, 'bo-', ...
+    wifi_delay, zigbee50byte25ms, 'r*-', ...
+    wifi_delay, zigbee100byte25ms, 'g^-', ...
+    wifi_delay, zigbee_throughput_no_interference_10_25ms, 'r--', ...
+    wifi_delay, zigbee_throughout_no_interference_50_25ms, 'r--', ...
+    wifi_delay, zigbee_throughput_no_interference_100_25ms, 'r--')
+grid on
+xlabel('wifi constant delay (ms)')
+ylabel('zigbee throughput (kbps)')
+legend('ZigBee packet size 10 byte','ZigBee packet size 50 byte', ...
+    'ZigBee packet size 100 byte')
+title('ZigBee throughput under WiFi512 interference')
+
+% (2) zigbee throughput vs. packet size 10, 100 byte with different delays
+packetsize = [10,50,100]';
+zigbee25ms = [zigbee10byte25ms; zigbee50byte25ms; zigbee100byte25ms];
+figure,
+hold on
+for i = 1:6
+    plot(packetsize, zigbee25ms(:,i), 'b*-')
+end
+hold off
+grid on
+xlabel('zigbee packet size (byte)')
+ylabel('zigbee throughput(kbps)')
+title('zigbee transmission interval 25ms')
+
+%-------------------------------------------------------------------------%
+% plot wifi throughput with different transmission interval under the same
+% zigbee payload 10 byte
+% zigbee throughput is linear with the zigbee transmission rate or inverse
+% with zigbee transmission interval
+figure,
+semilogy(wifi_delay, zigbee10byte10ms, 'r*-',...
+    wifi_delay, zigbee10byte25ms, 'bo-', ...
+    wifi_delay, zigbee10byte100ms, 'g^-')
+xlabel('wifi constant delay (ms)')
+ylabel('zigbee throughput (kbps)')
+legend('ZigBee transmission interval 10ms',...
+    'ZigBee transmission interval 25ms',...
+    'ZigBee transmission interval 100ms')
+title('ZigBee Packet Size 10 byte')
+grid on
+
+TI10 = [10, 25, 100]';
+zigbee10byte = [zigbee10byte10ms; zigbee10byte25ms; zigbee10byte100ms];
+figure,
+hold on
+for i = 1:6
+    semilogy(1000./TI10, zigbee10byte(:,i), 'bs-')
+end
+hold off
+grid on
+xlabel('zigbee packet per second')
+ylabel('zigbee throughput(kbps)')
+title('zigbee packet size 10 byte')
+
+% zigbee payload 100 byte
+figure,
+semilogy(wifi_delay, zigbee100byte25ms, 'r*-',...
+    wifi_delay, zigbee100byte50ms, 'bo-', ...
+    wifi_delay, zigbee10byte100ms, 'g^-')
+xlabel('wifi constant delay (ms)')
+ylabel('zigbee throughput (kbps)')
+legend('ZigBee transmission interval 25ms',...
+    'ZigBee transmission interval 50ms',...
+    'ZigBee transmission interval 100ms')
+title('ZigBee Packet Size 100 byte')
+grid on
+
+TI100 = [25, 50, 100]';
+tmp = 0 .* zigbee10byte10ms;
+zigbee100byte = [zigbee100byte25ms; zigbee100byte50ms; zigbee100byte100ms];
+figure,
+hold on
+for i = 1:6
+    semilogy(1000./TI100, zigbee100byte(:,i), 'bs-')
+end
+hold off
+grid on
+xlabel('zigbee packet per second')
+ylabel('zigbee throughput(kbps)')
+title('zigbee packet size 100 byte')
+
+%-------------------------------------------------------------------------%
+% combining the factors zigbee packet size and the transmission interval
+% which confirm our assumption that throughput = k * ps/ti
+figure,
+semilogy(wifi_delay, zigbee10byte10ms, 'bo-', ...
+    wifi_delay, zigbee100byte100ms, 'ro--')
+
+hold on,
+semilogy(wifi_delay, zigbee50byte25ms, 'bs-', ...
+    wifi_delay, zigbee100byte50ms, 'rs--')
+hold off
+
+xlabel('wifi constant delay (ms)')
+ylabel('zigbee throughput (kbps)')
+legend('ZigBee: 10 byte / 10ms',...
+    'ZigBee: 100 byte / 100ms', ...
+    'ZigBee: 50 byte / 25ms', ...
+    'ZigBee: 100 byte / 50ms')
+title('ZigBee throughput under WiFi512 interference')
+grid on
