1e3d291c13548d74431435f3a34bad612376fba6
Chen Wang <egbertcw@umich.edu>
2017-08-17 16:22:33 -0400

    Xbee transceiver

---
+++ ArduinoXBee/receiver.ino
@@ -0,0 +1,84 @@
+#include <XBee.h>   // Xbee lib
+#include <SoftwareSerial.h>
+
+XBee xbee = XBee();
+XBeeResponse response = XBeeResponse();
+SoftwareSerial serial(2,3);
+
+// create reusable response objects for responses we expect to handle 
+Rx16Response rx16 = Rx16Response();
+
+unsigned long otherError = 0; // other errors
+unsigned long pktLoss = 0; // number of packet loss
+unsigned long pktRcvd = 0; // number of packets received
+
+// calculating throughput
+unsigned perPktRcvd = 0;
+unsigned time_elapse = 0;
+double throughput = 0;
+
+const unsigned dataLen = 10; // we need to change the data length
+
+// print time, number of received packet and Xbee throughput
+void benchOutput() {
+  // calculate the throughput
+  throughput = perPktRcvd * 1.0 / time_elapse * dataLen * 8.0; // App TMT unit in Kbps 
+
+  Serial.print("time elapse: ");
+  Serial.println(time_elapse);
+  
+  Serial.print("pktRcvd: ");
+  Serial.println(pktRcvd);
+  
+  Serial.print("pktLoss: ");
+  Serial.println(pktLoss);
+  
+  Serial.print("otherError: ");
+  Serial.println(otherError);
+  
+  Serial.print("throughpout: ");
+  Serial.println(throughput); // Kbps
+}
+
+void setup() {
+  // start serial
+  Serial.begin(57600);
+  serial.begin(57600);
+  xbee.setSerial(serial);
+
+  Serial.println("Receiver Setup finished!");
+}
+
+// continuously reads packets, looking for RX16
+void loop() {
+    unsigned long start_time = millis();
+    xbee.readPacket();
+    
+    if (xbee.getResponse().isAvailable()) { // got something
+      // got a rx packet  
+      if (xbee.getResponse().getApiId() == RX_16_RESPONSE) { // 0x81 = 129
+        xbee.getResponse().getRx16Response(rx16);
+        pktRcvd += 1;
+        perPktRcvd += 1;
+      }
+      else {
+        pktLoss += 1;    
+      }
+    } 
+    else if (xbee.getResponse().isError()) {
+      //nss.print("Error reading packet.  Error code: ");  
+      //nss.println(xbee.getResponse().getErrorCode());
+      // or flash error led
+      // otherErrors include: CHECKSUM_FAILURE(1), 
+      // PACKET_EXCEEDS_BYTE_ARRAY_LENGTH(2), UNEXPECTED_START_BYTE(3)
+//      Serial.print(xbee.getResponse().getErrorCode());
+      otherError += 1;
+    } 
+
+    time_elapse += millis() - start_time;
+    if(time_elapse > 5000) {
+      benchOutput();
+      time_elapse = 0;
+      perPktRcvd = 0;
+    }
+}
---
+++ ArduinoXBee/transmitter.ino
@@ -0,0 +1,85 @@
+#include <XBee.h>
+#include <SoftwareSerial.h>
+
+/*
+This example is for Series 1 XBee
+Sends a TX16 request with the value of analogRead(pin5) and checks the status response for success
+Note: In my testing it took about 15 seconds for the XBee to start reporting success, so I've added a startup delay
+*/
+
+XBee xbee = XBee();
+SoftwareSerial serial(2,3);
+
+// allocate ten bytes
+uint8_t payload[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+
+unsigned pktTransmitted = 0;
+unsigned pktLoss = 0;
+unsigned otherError = 0;
+
+// 16-bit addressing: Enter address of remote XBee, typically the coordinator
+Tx16Request tx = Tx16Request(0x00, payload, sizeof(payload));
+
+TxStatusResponse txStatus = TxStatusResponse();
+
+unsigned perPeriod = 0;
+
+// print time, number of received packet and Xbee throughput
+void benchOutput() {
+  Serial.print("pktTransmitted: ");
+  Serial.print(pktTransmitted);
+  Serial.print("\t");
+  Serial.print("pktLoss: ");
+  Serial.print(pktLoss);
+  Serial.print("\t");
+  Serial.print("otherError: ");
+  Serial.println(otherError);
+}
+
+void setup() {
+  Serial.begin(57600);
+  serial.begin(57600);
+  xbee.setSerial(serial);
+  Serial.print("size of payload: ");
+  Serial.println(sizeof(payload));
+  Serial.println("Transmitter Setup finished!");
+}
+
+void loop() {
+
+    xbee.send(tx);
+  
+    // after sending a tx request, we expect a status response
+    // wait up to 5 seconds for the status response
+    if (xbee.readPacket(500)) {
+        // got a response!
+
+        // should be a znet tx status              
+      if (xbee.getResponse().getApiId() == TX_STATUS_RESPONSE) {
+         xbee.getResponse().getTxStatusResponse(txStatus);
+        
+         // get the delivery status, the fifth byte
+           if (txStatus.getStatus() == SUCCESS) {
+              // success.  time to celebrate
+              pktTransmitted += 1;
+
+           } else {
+              // the remote XBee did not receive our packet. is it powered on?
+              pktLoss += 1;
+           }
+        }      
+    } else if (xbee.getResponse().isError()) {
+      //nss.print("Error reading packet.  Error code: ");  
+      //nss.println(xbee.getResponse().getErrorCode());
+      // or flash error led
+        otherError += 1;
+    } 
+    
+    delayMicroseconds(500);
+    delay(8);
+    perPeriod += 1;
+    if(perPeriod == 1000) {
+      benchOutput();
+      perPeriod = 0;
+    }
+}
