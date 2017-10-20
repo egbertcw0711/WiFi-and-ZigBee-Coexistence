ddd13c1fb97da899c6511d6676ed84eb9729b333
egbertcw <egbertcw@umich.edu>
2017-10-20 10:06:59 -0400

    add another situation from the paper

--- MATLAB/blendtrans.m
+++ MATLAB/blendtrans.m
@@ -5,7 +5,7 @@ time_stamp = zeros(1,20000000); % simulate 100s and time interval 5us
 time_interval = 5; % us
 
 wifi_delays = [0, 100, 200, 300, 500, 1000, 2000, 4000, 5000,8000, 15000, 20000,...
-    30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000];
+    30000, 40000, 50000];%, 60000, 70000, 80000, 90000, 100000];
 throughputs = zeros(1, length(wifi_delays));
 
 % create WiFi signal
@@ -33,28 +33,49 @@ for j = 1:length(wifi_delays)
     ZB = [header,ZB];
     ZB = ZB(1:20000000);
 
-    C = zeros(1,20000000);
+    % C = zeros(1,20000000);
     count = 0;
     rcvd = 0;
     for i = 1:length(ZB)-1
-        if(ZB(i) == 1 && WiFi(i) == 0)
-            C(i) = 1;
-            if(ZB(i+1) == 1 && WiFi(i+1) == 0)
-                count = count + 1;
-                if(count == 121)
+        
+        % version 1: only wifi and zigbee packet collides at the beginning 
+        % and the end of zigbee packets are considered collision
+%         if(count == 0)
+%             if(ZB(i) == 1 && WiFi(i) == 0)
+%                 count = count + 1;
+%             end
+%         else
+%             if(ZB(i) == 1)
+%                 count = count + 1;
+%                 if(count == 122 && WiFi(i) == 0)
+%                     rcvd = rcvd + 1; 
+%                     count = 0;
+%                 elseif (count == 122 && WiFi(i) == 1)
+%                     count = 0;
+%                 end
+%             end
+%         end
+
+       % version 2: whenever wifi and zigbee packets overlap are considered
+       % collision
+       if(ZB(i) == 1 && WiFi(i) == 0)           
+           if(ZB(i+1) == 1 && WiFi(i+1) == 0)
+               count = count + 1;
+               if(count == 121)
                     rcvd = rcvd + 1;
-                end
-            else
+                    count = 0;
+               end
+           else
                 count = 0;
-            end 
-        end
+           end 
+       end
     end
 
     throughputs(j) = rcvd * 10 * 8 / 100000; % kbps
 end
 
 %% plotting
-plot(wifi_delays/1000, throughputs, 'r*-')
+semilogx(wifi_delays/1000, throughputs, 'r*-')
 xlabel('wifi delay (ms)')
 ylabel('zigbee throughput (kbps)')
 title('wifi and zigbee always transmit')
